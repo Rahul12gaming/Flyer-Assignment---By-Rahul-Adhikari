c4e8065f6a3fe641cdfb80349edd8175b4d5510c
Rahul adhikari <99792747+Rahul12gaming@users.noreply.github.com>
2024-07-12 18:15:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("button").addEventListener("click",()=>{
+        const contact=document.getElementById("contact-box");
+        contact.style.display="block"
+        
+    })
+        
+})
+
+function click()
+{
+    window.alert("Form Data Submit Successfully!")
+}
